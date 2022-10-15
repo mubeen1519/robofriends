7d3be831016a283e3051a41a8f5d2f029d605425
mubeen <mubeen1519@gmail.com>
2022-10-15 16:32:22 +0500

    Updates

---
+++ precache-manifest.dea0284191453a607a437c948ff107c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1df88ce6801bb44011cf",
+    "url": "/robofriends/static/css/main.b0878ed5.chunk.css"
+  },
+  {
+    "revision": "1df88ce6801bb44011cf",
+    "url": "/robofriends/static/js/main.1df88ce6.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/robofriends/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "919b7b636ccc3887910b",
+    "url": "/robofriends/static/css/2.3ac835ed.chunk.css"
+  },
+  {
+    "revision": "919b7b636ccc3887910b",
+    "url": "/robofriends/static/js/2.919b7b63.chunk.js"
+  },
+  {
+    "revision": "56c60b7404179c40f37f",
+    "url": "/robofriends/static/js/runtime~main.56c60b74.js"
+  },
+  {
+    "revision": "c43e57c7769ce93b3d787111fa4a192f",
+    "url": "/robofriends/index.html"
+  }
+];
